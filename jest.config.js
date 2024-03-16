932086837dd8e36a8e3330110c917917bb426df3
Valdir Silva <valdipiresba@gmail.com>
2024-03-16 10:50:11 -0300

    chore: add jest configs for mongodb

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,7 @@ module.exports = {
 
     // Automatically clear mock calls and instances between every test
     clearMocks: true,
+    preset: '@shelf/jest-mongodb',
 
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
