e61c15bbd3164748b394748f723bb608dfab3d31
Valdir Silva <valdipiresba@gmail.com>
2024-04-08 11:14:05 -0300

    refactor: clear database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -2,13 +2,17 @@ import { MongoHelper } from '../helpers/mongo-helper'
 import { AccountMongoRepository } from './account'
 
 describe('Account Mongo Repository', () => {
-
     beforeAll(async () => {
         await MongoHelper.connect(process.env.MONGO_URL)
     })
 
     afterAll(async () => {
         await MongoHelper.disconnect()
+    })
+
+    beforeEach(async () => {
+        const accountCollection = MongoHelper.getCollection('accounts')
+        await accountCollection.deleteMany({})
     })
 
     const makeSut = (): AccountMongoRepository => {
