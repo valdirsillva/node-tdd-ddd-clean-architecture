1e68f907037d5c949098f2e816db33023f2ce3a3
Valdir Silva <valdipiresba@gmail.com>
2024-04-08 12:34:46 -0300

    feat:  ensure return json as default content type

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,10 @@
 import { Express } from 'express'
 import { cors } from '../middlewares/cors'
+import { contentType } from '../middlewares/content-type'
 import { bodyParser } from '../middlewares/body-parser'
+
 export default (app: Express): void => {
     app.use(bodyParser)
     app.use(cors)
+    app.use(contentType)
 }
