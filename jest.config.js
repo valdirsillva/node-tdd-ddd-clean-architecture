08952fbd4f9f645f56dd6c1f8393ae6f78758a83
Valdir Silva <valdipiresba@gmail.com>
2024-04-13 16:18:54 -0300

    feat: ensure mongodb reconnect is client is down

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,10 @@
 module.exports = {
-    // All imported modules in your tests should be mocked automatically
-    // automock: false,
-
-    // Stop running tests after `n` failures
-    // bail: 0,
-
-    // The directory where Jest should store its cached dependency information
-    // cacheDirectory: "/tmp/jest_rs",
-
-    // Automatically clear mock calls and instances between every test
 
     roots: ['<rootDir>/src'],
-    collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.ts',
+        '!<rootDir>/src/main/**'
+    ],
     coverageDirectory: 'coverage',
     testEnvironment: "node",
     preset: '@shelf/jest-mongodb',
