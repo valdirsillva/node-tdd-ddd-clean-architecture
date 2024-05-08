07c7982df358472059b83c391bdeda551f80b899
Valdir Silva <valdipiresba@gmail.com>
2024-05-07 21:26:03 -0300

    test: ensure DbAuthentication returns a token on success

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -130,4 +130,10 @@ describe('DbAuthentication UseCase', () => {
         const promise = sut.auth(makeFakeAuthentication())
         await expect(promise).rejects.toThrow()
     })
+
+    test('Should call TokenGenerator returns token', async () => {
+        const { sut } = makeSut()
+        const accessToken = await sut.auth(makeFakeAuthentication())
+        expect(accessToken).toBe('any_token')
+    })
 })
