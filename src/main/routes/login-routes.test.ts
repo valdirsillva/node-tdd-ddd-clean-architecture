aad04533ace19e7267c70554e9ccaed08d63c5be
Valdir Silva <valdipiresba@gmail.com>
2024-06-02 16:25:02 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -51,5 +51,16 @@ describe('Login Routes', () => {
                 .expect(200)
 
         })
+
+        test('Should return 401 on login', async () => {
+            await request(app)
+                .post('/api/login')
+                .send({
+                    email: 'valdirpiresba@gmail.com',
+                    password: '123'
+                })
+                .expect(401)
+
+        })
     })
 })
