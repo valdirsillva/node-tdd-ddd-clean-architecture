03895902ca4c629ff9e7798a994711878643581d
Valdir Silva <valdipiresba@gmail.com>
2024-06-02 15:03:41 -0300

    feat: ensure AccountMongoRepository updates the account accessToken on update accessToken success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -3,13 +3,16 @@ import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { AccountModel } from '../../../../domain/models/account'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
+import { UpdateAccessTokenRepository } from '../../../../data/protocols/db/update-access-token-repository'
+import { ObjectId } from 'mongodb'
 
-export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository {
     async add(accountData: AddAccountModel): Promise<AccountModel> {
         const accountCollection = await MongoHelper.getCollection('accounts')
         const result = await accountCollection.insertOne(accountData)
         return MongoHelper.map(result, accountData)
     }
+
     async loadByEmail(email: string): Promise<AccountModel> {
         const accountCollection = await MongoHelper.getCollection('accounts')
         const account = await accountCollection.findOne({ email })
@@ -18,5 +21,13 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
             const { _id, name, password } = account
             return account && { id: _id.toString(), name, email, password }
         }
+    }
+
+    async updateAccessToken(id: string, token: string): Promise<void> {
+        const accountCollection = await MongoHelper.getCollection('accounts')
+        await accountCollection.updateOne(
+            { _id: new ObjectId(id) },
+            { $set: { accessToken: token } }
+        )
     }
 }
