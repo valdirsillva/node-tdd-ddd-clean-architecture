18776857dbbfd54b1a9bcfbb27a5b9c4823aa485
Valdir Silva <valdipiresba@gmail.com>
2024-06-02 16:16:04 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
     mongoUrl: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/api-clean-nodejs',
-    port: process.env.PORT || 5000
+    port: process.env.PORT || 5000,
+    jwtSecret: process.env.JWT_SECRET || 'XYZ$XPTO'
 }
