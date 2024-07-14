2ee62e641365573f9971a0ed050e7277d2fe5c2b
Valdir Silva <valdipiresba@gmail.com>
2024-07-14 19:02:23 -0300

    test: ensure AuthMiddleware returns 200 if LoadAccountByToken succeeds

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,4 +1,4 @@
-import { forbidden } from '../helpers/http/http-helper'
+import { forbidden, ok } from '../helpers/http/http-helper'
 import { AccessDeniedError } from '../errors'
 import { AuthMiddleware } from './auth-middleware'
 import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
@@ -55,12 +55,18 @@ describe('Auth Middleware', () => {
         expect(loadSpy).toHaveBeenCalledWith('any_token')
     })
 
-    test('Should return 403 if oadAccountByToken returns null', async () => {
+    test('Should return 403 if LoadAccountByToken returns null', async () => {
         const { sut, loadAccountByTokenStub } = makeSut()
         jest.spyOn(loadAccountByTokenStub, 'load').mockReturnValueOnce(
             new Promise(resolve => resolve(null))
         )
         const httpResponse = await sut.handle({})
         expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+    })
+
+    test('Should return 200 if LoadAccountByToken returns an account', async () => {
+        const { sut } = makeSut()
+        const httpResponse = await sut.handle(makeFakeRequest())
+        expect(httpResponse).toEqual(ok({ accountId: 'valid_id' }))
     })
 })
