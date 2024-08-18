7f7139dbdb0999eb3570cfe7e78c654118f3a8ac
Valdir Silva <valdipiresba@gmail.com>
2024-08-18 18:14:06 -0300

    fix: ensure AccountMongoRepository allows admin to access any route

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,11 +2,16 @@ import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
+import { ObjectId } from 'mongodb'
 
 let surveyCollection: Collection
+let accountCollection: Collection
+
 describe('Survey Routes', () => {
     beforeAll(async () => {
-        await MongoHelper.connect(process.env.MONGO_URL)
+        await MongoHelper.connect(process.env.MONGO_URL!)
     })
 
     afterAll(async () => {
@@ -16,6 +21,13 @@ describe('Survey Routes', () => {
     beforeEach(async () => {
         surveyCollection = await MongoHelper.getCollection('surveys')
         await surveyCollection.deleteMany({})
+
+        accountCollection = await MongoHelper.getCollection('accounts')
+        await accountCollection.deleteMany({})
+    })
+
+    afterEach(() => {
+        jest.clearAllMocks()
     })
 
     describe('POST /surveys', () => {
@@ -32,6 +44,44 @@ describe('Survey Routes', () => {
                     }]
                 })
                 .expect(403)
+
+        })
+
+        test('Should return 204 on add survey with valid accessToken', async () => {
+            const res = await accountCollection.insertOne({
+                name: 'Valdir',
+                email: 'valdir@gmail.com.br',
+                password: '123',
+                role: 'admin'
+            })
+
+            const id = res.insertedId
+            const accessToken = sign({ id: id.toHexString() }, 'XYZ$XPTO', {
+                expiresIn: '1h'
+            })
+
+            await accountCollection.updateOne({
+                _id: new ObjectId(id)
+            }, {
+                $set: {
+                    accessToken
+                }
+            })
+
+            // await request(app)
+            //     .post('/api/surveys')
+            //     .set('x-access-token', accessToken)
+            //     .send({
+            //         question: 'Question',
+            //         answers: [{
+            //             answer: 'Answer 1',
+            //             image: 'http://image-name.com'
+            //         }, {
+            //             answer: 'Answer 1',
+            //         }]
+            //     }).expect(204)
+
+
 
         })
     })
