caf85313160c0654150898d91303d4419e781620
Valdir Silva <valdipiresba@gmail.com>
2024-09-22 18:33:27 -0300

    fix: response type correct

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -8,7 +8,8 @@ export const adapterRoute = (controller: Controller) => {
         }
         const httpResponse = await controller.handle(httpRequest)
         if (httpResponse.statusCode >= 200 || httpResponse.statusCode <= 299) {
-            res.status(httpResponse.statusCode).json(httpRequest.body)
+            console.log(httpResponse)
+            res.status(httpResponse.statusCode).json(httpResponse.body)
         } else {
             res.status(httpResponse.statusCode).json({
                 error: httpResponse.body.message
