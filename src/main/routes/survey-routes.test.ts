764b2ff8dbd7bc062a14ea6637ad3a0aff66a752
Valdir Silva <valdirpiresba@gmail.com>
2024-10-12 16:38:12 -0300

    refactor: move acessToken creation to a factory method

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -9,115 +9,91 @@ import { ObjectId } from 'mongodb'
 let surveyCollection: Collection
 let accountCollection: Collection
 
+const makeAcessToken = async (): Promise<string> => {
+	const res = await accountCollection.insertOne({
+		name: 'Valdir',
+		email: 'valdirpiresba@gmail.com',
+		password: '123',
+		role: 'admin'
+	})
+	const id = res.insertedId.toString()
+	const accessToken = sign({ id }, env.jwtSecret)
+	await accountCollection.updateOne({
+		_id: res.insertedId
+	}, {
+		$set: {
+			accessToken
+		}
+	})
+	return accessToken
+}
+
 describe('Survey Routes', () => {
-    beforeAll(async () => {
-        await MongoHelper.connect(process.env.MONGO_URL!)
-    })
+	beforeAll(async () => {
+		await MongoHelper.connect(process.env.MONGO_URL!)
+	})
 
-    beforeEach(async () => {
-        surveyCollection = await MongoHelper.getCollection('surveys')
-        await surveyCollection.deleteMany({})
+	beforeEach(async () => {
+		surveyCollection = await MongoHelper.getCollection('surveys')
+		await surveyCollection.deleteMany({})
 
-        accountCollection = await MongoHelper.getCollection('accounts')
-        await accountCollection.deleteMany({})
-    })
+		accountCollection = await MongoHelper.getCollection('accounts')
+		await accountCollection.deleteMany({})
+	})
 
-    afterAll(async () => {
-        await MongoHelper.disconnect()
-    })
+	afterAll(async () => {
+		await MongoHelper.disconnect()
+	})
 
-    describe('POST /surveys', () => {
-        test('Should return 403 on add survey withount accessToken', async () => {
-            await request(app)
-                .post('/api/surveys')
-                .send({
-                    question: 'Question',
-                    answers: [{
-                        answer: 'Answer 1',
-                        image: 'http://image-name.com'
-                    }, {
-                        answer: 'Answer 1',
-                    }]
-                })
-                .expect(403)
+	describe('POST /surveys', () => {
+		test('Should return 403 on add survey withount accessToken', async () => {
+			await request(app)
+				.post('/api/surveys')
+				.send({
+					question: 'Question',
+					answers: [{
+						answer: 'Answer 1',
+						image: 'http://image-name.com'
+					}, {
+						answer: 'Answer 1',
+					}]
+				})
+				.expect(403)
 
-        })
+		})
 
-        test('Should return 204 on add survey with valid accessToken', async () => {
-            const res = await accountCollection.insertOne({
-                name: 'Valdir',
-                email: 'valdirpiresba@gmail.com',
-                password: '123',
-                role: 'admin'
-            })
+		test('Should return 204 on add survey with valid accessToken', async () => {
+			const accessToken = await makeAcessToken()
+			await request(app)
+				.post('/api/surveys')
+				.set('x-access-token', accessToken)
+				.send({
+					question: 'Question',
+					answers: [{
+						answer: 'Answer 1',
+						image: 'http://image-name.com'
+					}, {
+						answer: 'Answer 2'
+					}]
+				})
+				.expect(204)
 
-            const id = res.insertedId.toString()
-            const accessToken = sign({ id }, env.jwtSecret)
+		})
+	})
 
-            await accountCollection.updateOne({
-                _id: res.insertedId
-            }, {
-                $set: {
-                    accessToken
-                }
-            })
+	describe('GET /surveys', () => {
+		test('Should return 403 on load surveys withount accessToken', async () => {
+			await request(app)
+				.get('/api/surveys')
+				.expect(403)
+		})
 
-            // Bug Fix
-            await request(app)
-                .post('/api/surveys')
-                .set('x-access-token', accessToken)
-                .send({
-                    question: 'Question',
-                    answers: [{
-                        answer: 'Answer 1',
-                        image: 'http://image-name.com'
-                    }, {
-                        answer: 'Answer 2'
-                    }]
-                })
-                .expect(204)
-            
-        })
-    })
-
-    describe('GET /surveys', () => {
-        test('Should return 403 on load surveys withount accessToken', async () => {
-            await request(app)
-                .get('/api/surveys')
-                .expect(403)
-        })
-
-        test('Should return 200 on load surveys with valid accessToken', async () => {
-            const res = await accountCollection.insertOne({
-                name: 'Valdir',
-                email: 'valdirpiresba@gmail.com',
-                password: '123',
-                role: 'admin'
-            })
-
-            const id = res.insertedId
-            const accessToken = sign({ id: res.insertedId.toString() }, env.jwtSecret)
-            await accountCollection.updateOne({
-                _id: id
-            }, {
-                $set: {
-                    accessToken
-                }
-            })
-
-            await surveyCollection.insertMany([{
-                question: 'any_question',
-                answers: [{
-                    image: 'any_image',
-                    answer: 'any_answer'
-                }],
-                date: new Date()
-            }])
-
-            await request(app)
-                .get('/api/surveys')
-                .set('x-access-token', accessToken)
-                .expect(200)
-        })
-    })
+		test('Should return 204 on load surveys with valid accessToken', async () => {
+			const accessToken = await makeAcessToken()
+			await request(app)
+				.get('/api/surveys')
+				.set('x-access-token', accessToken)
+				.expect(204)
+		})
+	})
 })
