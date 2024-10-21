8a64d30907e8da5804a01c44e25c50477f28bb34
Valdir Silva <valdirpiresba@gmail.com>
2024-10-21 19:56:22 -0300

    test: ensure DbSaveSurveyResult calls SaveSurveyResultRepository with correct values

---
+++ src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
@@ -0,0 +1,59 @@
+import MockDate from 'mockdate'
+import { SaveSurveyResultRepository, SaveSurveyResultModel, SurveyResultModel } from './db-save-survey-result-protocols'
+import { DbSaveSurveyResult } from './db-save-survey-result'
+
+const makeFakeSurveyResult = (): SurveyResultModel => ({
+    id: 'any_id',
+    accountId: 'any_account_id',
+    surveyId: 'any_survey_id',
+    answer: 'any_answer',
+    date: new Date()
+})
+
+const makeFakeSurveyResultData = (): SaveSurveyResultModel => ({
+    accountId: 'any_account_id',
+    surveyId: 'any_survey_id',
+    answer: 'any_answer',
+    date: new Date()
+})
+
+type SutTypes = {
+    sut: DbSaveSurveyResult
+    saveSurveyResultRepositoryStub: SaveSurveyResultRepository
+}
+
+const makeSaveSurveyResultRepository = (): SaveSurveyResultRepository => {
+    class SaveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
+        async save(data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+            return new Promise(resolve => resolve(makeFakeSurveyResult()))
+        }
+    }
+    return new SaveSurveyResultRepositoryStub()
+}
+
+const makeSut = (): SutTypes => {
+    const saveSurveyResultRepositoryStub = makeSaveSurveyResultRepository()
+    const sut = new DbSaveSurveyResult(saveSurveyResultRepositoryStub)
+    return {
+        sut,
+        saveSurveyResultRepositoryStub
+    }
+}
+
+describe('DbSaveSurveyResult Usecase', () => {
+    beforeAll(() => {
+        MockDate.set(new Date())
+    })
+
+    afterAll(() => {
+        MockDate.reset()
+    })
+
+    test('Should call SaveSurveyResultRepository with correct values', async () => {
+        const { sut, saveSurveyResultRepositoryStub } = makeSut()
+        const addSpy = jest.spyOn(saveSurveyResultRepositoryStub, 'save')
+        const surveyResultData = makeFakeSurveyResultData()
+        await sut.save(surveyResultData)
+        expect(addSpy).toHaveBeenCalledWith(surveyResultData)
+    })
+})
