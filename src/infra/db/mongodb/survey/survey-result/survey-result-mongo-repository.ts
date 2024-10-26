4a3e85cac89f562f6bf1410aa1a60b90028ae8d4
Valdir Silva <valdirpiresba@gmail.com>
2024-10-26 14:50:31 -0300

    feat: ensure SurveyResultMongoRepository add surveyResult if not exists with accountId and surveyId

---
+++ src/infra/db/mongodb/survey/survey-result/survey-result-mongo-repository.ts
@@ -0,0 +1,34 @@
+import { SaveSurveyResultModel, SaveSurveyResultRepository, SurveyResultModel } from '@/data/usecases/save-survey-result/db-save-survey-result-protocols'
+import { MongoHelper } from '../../helpers/mongo-helper'
+
+export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+    async save(data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+        const surveyResultCollection = await MongoHelper.getCollection('surveyResults')
+        const res = await surveyResultCollection.findOneAndUpdate({
+            surveyId: data.surveyId,
+            accountId: data.accountId
+        }, {
+            $set: {
+                answer: data.answer,
+                date: data.date
+            }
+        }, {
+            upsert: true,
+            returnDocument: "after"
+        })
+
+        if (!res.value) {
+            throw new Error('Failed to save survey result');
+        }
+
+       const response: SurveyResultModel = {
+        id: res.value._id.toHexString(),
+        accountId: res.value.accountId,
+        surveyId: res.value.surveyId,
+        answer: res.value.answer,
+        date: res.value.date
+       }
+
+       return response
+    }
+}
