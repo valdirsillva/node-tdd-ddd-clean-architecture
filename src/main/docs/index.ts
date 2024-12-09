830c00f1fc020f009509bff041fac17b3731af3a
Valdir Silva <valdirpiresba@gmail.com>
2024-12-09 20:35:06 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,3 +1,7 @@
+import { loginPath } from './paths/login-path'
+import { accountSchema } from './schemas/account-schema'
+import { loginParamsSchema } from './schemas/login-params-schema'
+
 
 export default {
     openapi: '3.0.0',
@@ -5,5 +9,18 @@ export default {
         title: 'Clean Node API',
         description: 'API do curso clean architecture',
         version: '1.0.0'
+    },
+    servers: [{
+        url: '/api'
+    }],
+    tags: [{
+        name: 'Login'
+    }],
+    paths: {
+        '/login': loginPath
+    },
+    schemas: {
+        account: accountSchema,
+        loginParams: loginParamsSchema
     }
 }
