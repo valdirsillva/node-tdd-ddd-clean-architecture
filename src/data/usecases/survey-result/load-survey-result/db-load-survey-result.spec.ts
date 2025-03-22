0c693753680e0ff933ac3f7bc3d11e91e9de70a4
Valdir Silva <valdirpiresba@gmail.com>
2025-03-22 18:05:54 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -3,15 +3,32 @@ import { SurveyResultModel } from '../save-survey-result/db-save-survey-result-p
 import { DbLoadSurveyResult } from './db-load-survey-result'
 import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
 
+type SutTypes = {
+    sut: DbLoadSurveyResult
+    loadSurveyResultRepositoryStub: LoadSurveyResultRepository
+}
+
+const mockLoadSurveyResultRepository = (): LoadSurveyResultRepository => {
+  class LoadSurveyResultRepositoryStub implements LoadSurveyResultRepository {
+    async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
+      return Promise.resolve(mockSurveyModelResult())
+    }
+  }
+  return new LoadSurveyResultRepositoryStub()
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyResultRepositoryStub = mockLoadSurveyResultRepository()
+  const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub)
+  return {
+    sut, 
+    loadSurveyResultRepositoryStub
+  }
+}
+
 describe('DBLoadSurveyResult UseCase', () => {
   test('Should call LoadSurveyResultRepository', async () => {
-    class LoadSurveyResultRepositoryStub implements LoadSurveyResultRepository {
-      async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
-        return Promise.resolve(mockSurveyModelResult())
-      }
-    }
-    const loadSurveyResultRepositoryStub = new LoadSurveyResultRepositoryStub()
-    const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub)
+    const { sut, loadSurveyResultRepositoryStub } = makeSut()
     const loadBySurveyIdSpy = jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId')
     await sut.load('any_survey_id')
     expect(loadBySurveyIdSpy).toHaveBeenLastCalledWith('any_survey_id')
